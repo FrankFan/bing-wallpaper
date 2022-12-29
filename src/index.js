05ce5eec02463fe094ac9e59c36a122542c45a0d
FrankFan <fanyong@gmail.com>
2022-12-29 21:16:11 +0800

    feat: update url

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const { writeUrlToFile } = require('./fileHepler')
 
   // 坑2: 获取dom元素无效的问题
   // https://www.urlbox.io/puppeteer-wait-for-page-load
-  await page.goto('https://bing.com', {
+  await page.goto('https://cn.bing.com/', {
     waitUntil: 'networkidle2',
   })
 

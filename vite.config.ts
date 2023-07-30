a4ff70feacd75eef12a200ca912608074adf0779
FrankFan <fanyong@gmail.com>
2023-07-30 17:13:18 +0800

    feat: archive report 0

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import process from 'process';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/bing-wallpaper/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        process.env.VISUALIZER === 'show' &&
+          visualizer({
+            open: true,
+            gzipSize: true,
+          }),
+      ],
+    },
+  },
 });

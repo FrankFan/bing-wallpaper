c42e855f72a1954e90684fe6af77c3bb2eb04501
fanyong <fanyong@gmail.com>
2025-05-28 10:52:14 +0800

    feat: add official api

--- src/index.js
+++ src/index.js
@@ -1,64 +1,33 @@
 const puppeteer = require('puppeteer')
 const { writeUrlToFile } = require('./fileHepler')
+const axios = require('axios')
 
 ;(async () => {
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    headless: true, // 是否无头
-    devtools: true,
-  })
-  // 坑1：这样可以解决2个tab的问题
-  const page = (await browser.pages())[0]
+  try {
+    // 使用 Bing 壁纸 API
+    const response = await axios.get('https://www.bing.com/HPImageArchive.aspx', {
+      params: {
+        format: 'js',
+        idx: 1,  // 0 表示今天
+        n: 2,    // 获取1张图片
+        mkt: 'zh-CN'
+      }
+    });
 
-  // 坑2: 获取dom元素无效的问题
-  // https://www.urlbox.io/puppeteer-wait-for-page-load
-  // 国内IP访问 bing.com 会自动跳转到 cn.bing.com，太坑了
-  // 导致本地和服务器上的行为不一致
-  await page.goto('https://www.bing.com/?mkt=zh-CN', {
-    waitUntil: 'networkidle2',
-  })
+    const imageData = response.data.images[0];
+    console.log('获取到的图片数据:', imageData);
 
-  // 坑3: 必须等到具体元素出来以后再操作
-  // await page.waitForSelector('#iotd_title', { visible: true })
+    const bingWallpaperObj = {
+      bgUrl: `https://www.bing.com${imageData.url}`,
+      title: imageData.title,
+      credit: imageData.copyright,
+      desc: imageData.copyright
+    };
 
-  const bingWallpaperObj = await page.evaluate(async () => {
-    function getBingBackgroundImage() {
-      const theDiv = document.querySelector('.img_cont')
-      // 获取 div 的 background-image 属性
-      // url("https://cn.bing.com/th?id=OHR.StorrRocks_ZH-CN4956679462_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&qlt=50")
-      const bgImage = window.getComputedStyle(theDiv)['backgroundImage']
-
-      // 通过正则提取url
-      const regObj = bgImage.match(/url\("(\S*)"\)/)
-      return regObj && regObj.length > 1 && regObj[1]
-    }
-
-    function getTitle() {
-      const selector = '#headline'
-      return document.querySelector(selector).innerText
-    }
-
-    function getCredit() {
-      const selector = '#copyright'
-      return document.querySelector(selector).innerText
-    }
-
-    function getDesc() {
-      const selector = '.musCardCont h3 a'
-      return document.querySelector(selector).innerText
-    }
-
-    return Promise.resolve({
-      bgUrl: getBingBackgroundImage(),
-      title: getTitle(),
-      credit: getCredit(),
-      desc: getDesc(),
-    })
-  })
-
-  // await page.screenshot({ path: 'bing.png' })
-
-  console.log(bingWallpaperObj)
-  writeUrlToFile(bingWallpaperObj)
-  await browser.close()
+    console.log('壁纸信息:', bingWallpaperObj);
+    // writeUrlToFile(bingWallpaperObj);
+  } catch (error) {
+    console.error('获取壁纸信息失败:', error);
+  }
 })()
+
